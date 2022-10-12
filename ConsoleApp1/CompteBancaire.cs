0e82e2db67886effa9ccd08f3aa28a3e4c7097bd
achref_ama <amami.achref@gmail.com>
2022-10-12 20:11:23 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleApp1/CompteBancaire.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class CompteBancaire
+    {
+        public string NomClient { get; set; }
+        public Double SoldeClient { get; set; }
+
+        public CompteBancaire( string nom, double salaire)
+        {
+            this.NomClient= nom;
+            this.SoldeClient= salaire;
+        }
+
+        public void afficheInfoClient ()
+        {
+            Console.WriteLine(NomClient);
+            Console.WriteLine(SoldeClient);
+
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CompteBancaire cp = new CompteBancaire("ali becha",2500);
+            cp.afficheInfoClient();
+            cp.NomClient = "achref";
+            cp.SoldeClient = 2500;
+
+        }
+    }
+}
